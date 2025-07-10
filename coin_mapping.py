0b6e04a77a48369b4fd5f6a38c4f596f27cca6a3
p06636 <p06636@naver.com>
2025-07-10 23:40:12 +0900

    Add files via upload

---
+++ coin_mapping.py
@@ -0,0 +1,8 @@
+# CoinGecko ID Mapping Dictionary
+coin_mapping = {
+'ADA': 'cardano', 'DOT': 'polkadot', 'RPL': 'rocket-pool', 'SNT': 'status',
+'BTC': 'bitcoin', 'ETH': 'ethereum', 'AVAX': 'avalanche-2', 'LINK': 'chainlink',
+'AAVE': 'aave', 'HBAR': 'hedera-hashgraph', 'FET': 'fetch-ai', 'INJ': 'injective-protocol',
+'NEIRO': 'neurai', 'AI16Z': 'ai16z', 'SUNDOG': 'sundog'
+# Add remaining 25+ tokens here
+}
